2599035b703233f50e0c84fb4f37c5c18f13ff7a
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-26 22:21:39 +0700

    Add files via upload
    Bài tập 1 : Chuyển đổi tiền (Sử dụng optional parameter)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Bài toán chuyển đổi tiền tệ
+using System;
+namespace TranQuangThong
+{
+    class Program
+    {
+        public static double ChuyenDoiTien(double soTien, double tyGia = 23000)
+        {
+            return soTien * tyGia;
+        }
+        public static void Main()
+        {
+            Console.Write("Nhap so tien can chuyen doi: ");
+            double soTien = Convert.ToDouble(Console.ReadLine());
+            //Hàm với tý giá mặc định
+            Console.WriteLine($"Da chuyen {soTien}$ thanh {ChuyenDoiTien(soTien)} VND");
+            //Hàm với tỷ giá tùy chỉnh
+            Console.WriteLine($"Da chuyen {soTien}$ thanh {ChuyenDoiTien(soTien,24000)} VND");
+        }
+    }
+}
