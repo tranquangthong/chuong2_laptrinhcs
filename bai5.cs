613f0a82e5f131ebeb2da8e5bc6986c81c9aac6e
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-27 19:15:30 +0700

    Update bai5.cs
    Tạo lớp point

--- bai5.cs
+++ bai5.cs
@@ -7,6 +7,22 @@ namespace TranQuangThong
     BloodRed,
     God
   }
+  public class Point
+  {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public PointColor Color { get; set; }
+    public Point (int x, int y, Point color) 
+    {
+      X = x;
+      Y = y;
+      Color = color;
+    }
+    public void DisplayStatus()
+    {
+      Console.WriteLine($"Point ({X}, {Y} - Color: {Color})");
+    }
+  }
   class Program
   {
     static void Main()
