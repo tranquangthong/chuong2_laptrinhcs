8456a2e3d7dce2bbc502fec546e558e6298ef745
tranquangthong <148887480+tranquangthong@users.noreply.github.com>
2025-02-27 19:00:01 +0700

    Create bai5.cs
    Tạo chương trình
    Tạo Enum để định nghĩa màu của điểm

---
+++ bai5.cs
@@ -0,0 +1,18 @@
+using System;
+namespace TranQuangThong
+{
+  public enum PoinColor
+  {
+    LightBlue,
+    BloodRed,
+    God
+  }
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
+
